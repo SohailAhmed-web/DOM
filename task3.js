2e354033975fef58711836fe56e843e5b37ea482
SohailAhmed-web <sohail.bscsf19@iba-suk.edu.pk>
2023-10-05 22:04:42 +0500

    Task 3 done

---
+++ task3.js
@@ -0,0 +1,11 @@
+function js_style() {
+    // Get the paragraph element by its id
+    const paragraph = document.getElementById('text');
+
+    // Modify the style properties
+    paragraph.style.fontSize = '24px'; // Change font size
+    paragraph.style.fontFamily = 'Arial, sans-serif'; // Change font family
+    paragraph.style.color = 'blue'; // Change text color
+    paragraph.style.backgroundColor = 'yellow'; // Change background color
+    // You can add more style properties as needed
+  }
